0b1c09757c272d871a0bc4f5ef0a46fcbdb27781
adarshkunwar <adarshkunwarofficial@gmail.com>
2024-07-14 07:03:26 +0545

    Starting for single category choice in product

--- src/components/shop-page/sidebar/sidebar-item.tsx
+++ src/components/shop-page/sidebar/sidebar-item.tsx
@@ -2,9 +2,19 @@
 
 import { Checkbox } from "@/components/ui/checkbox";
 import { SideBarSingleItem } from "@/types/sidebar";
+import { useParams, usePathname, useSearchParams } from "next/navigation";
+import { useRouter } from "next/router";
 import { useState } from "react";
 
+export const CHOSEN_CATEGORY = "category"
+
 export function SingleCheckBox({ isTrue, content }: SideBarSingleItem) {
+  const router = useRouter()
+  const searchParams = useSearchParams()
+  const pathname = usePathname()
+  const chosenCategory = Number(searchParams.get(CHOSEN_CATEGORY ?? 'all'))
+
+
   const [isTrueS, setIsTrueS] = useState(isTrue);
 
   const handleCheck = () => {
