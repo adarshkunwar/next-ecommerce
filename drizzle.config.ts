432b34f0fe7359e70efcc385a942765f4d1ede6d
adarshkunwar <adarshkunwarofficial@gmail.com>
2024-07-18 11:45:37 +0545

    Added Drizzle

---
+++ drizzle.config.ts
@@ -0,0 +1,10 @@
+require("dotenv").config();
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  schema: "./app/db/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.POSTGRES_URL!,
+  },
+});
